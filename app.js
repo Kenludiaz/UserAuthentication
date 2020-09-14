e94c4c3304c946de489bb43469fcc43030d50899
Kenneth Lu Diaz <ludiazken@gmail.com>
2020-09-14 09:06:21 -0400

    Connected to database..

--- app.js
+++ app.js
@@ -1,7 +1,21 @@
 const express = require('express');
 const app = express();
 const users = require('./route/users.route');
+const mongoose = require('mongoose');
 
+
+// ========Middleware========
 app.use('/', users);
+
+
+
+
+// ========Database========
+let mongoDB = 'mongodb+srv://Ken:cheese11@cluster0.lcalo.mongodb.net/userAuthentication?retryWrites=true&w=majority';
+mongoose.connect(mongoDB, { useNewUrlParser: true });
+
+let db = mongoose.connection;
+
+db.on('error', console.error.bing(console, 'MongoDB connection error:'));
 
 module.exports = app;
---
+++ models/UserSchema.js
@@ -0,0 +1,12 @@
+let mongoose = require('mongoose');
+
+let Schema = mongoose.Schema;
+
+let UserSchema = {
+    y=username: _username,
+    Password: _password,
+    Age: _age,
+    FullName: _fullname,
+    Email: _email
+}
+
