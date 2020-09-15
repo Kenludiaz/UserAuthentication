e3c76533d4bd1bdeb5d430cce065986faf24c504
Kenneth Lu Diaz <ludiazken@gmail.com>
2020-09-15 09:36:18 -0400

    Made a function to generate Authentication Tokens

--- models/user.js
+++ models/user.js
@@ -1,13 +1,33 @@
-let mongoose = require('mongoose');
+const mongoose = require('mongoose');
+const jwt = require('jsonwebtoken');
+const Joi = require('joi');
+const config = require('config')
+
 
 let Schema = mongoose.Schema;
 
 let UserSchema = new Schema ({
-    username: {type: String, required: [true, "Please enter a valid username or password."]},
-    password: {type: String, required: [true, "Please enter a valid username or password."]},
-    age: {type: Number, min: 16},
-    full_name: {type: String},
-    email: {type: String}
+    name: {type: String, required: true, minlength: 3, maxlength: 50},
+    password: {type: String, required: [true, "Please enter a valid username or password."], minlength: 3, maxlength: 255},
+    email: {type: String, required: [true, "Please enter a valid username or password."], minlength: 5, maxlength: 255, unique: true},
+
+    isAdmin: Boolean
 });
+
+UserSchema.methods.generateAuthToken = function() {
+    const token = jwt.sign({_id: this._id, isAdmin: this.isAdmin }, config.get('myprivatekey'))
+    return token;
+}
+
+function validateUser(user) {
+    const schema = {
+        name: Joi.string().min(3).max(50).required(),
+        email: Joi.string().min(5).max(255).required().email(),
+        password: Joi.string()
+    }
+}
+
+
+
 
 module.exports = mongoose.model('UserModel', UserSchema);
